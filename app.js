fe3ee60309033645208ecc08753ddb6430653984
Kimathi Kaai <kimathi_kaai@cooperators.ca>
2018-08-15 12:07:36 -0700

    Printing out information pulled from geocode google api

--- app.js
+++ app.js
@@ -6,5 +6,7 @@ var requestOptions = {
 };
 
 request( requestOptions, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
