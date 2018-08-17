beea019b073dbab94fdaecf9d6691ebb357be20f
Kimathi Kaai <kimathi_kaai@cooperators.ca>
2018-08-17 14:17:49 -0700

    Tested request errors and provided user on information about their request

--- app.js
+++ app.js
@@ -14,18 +14,28 @@ const argv = yargs
   .alias('help','h')
   .argv;
 
-var encdodedAddress = encodeURIComponent(argv.a);
+var encodedAddress = encodeURIComponent(argv.a);
 console.log(argv);
-console.log(`Encoded Address: ${encdodedAddress}`);
+console.log(`Encoded Address: ${encodedAddress}`);
 
 
 var requestOptions = {
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encdodedAddress}`,
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 };
 
+console.log(`Endpoint URL: ${requestOptions.url}`);
+
 request( requestOptions, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  //Checks if the error object exists
+  //This won't tell us that there was a error with the address
+  if(error){
+    console.log('Unable to connect Google Service.');
+  } else if (body.status === 'ZERO_RESULTS'){
+    console.log('Unable to find that address');
+  } else if (body.status == 'OK') {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  }
 });
